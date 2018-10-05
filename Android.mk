2127ab2884d70ff0321d4e1e6d82ac18ae25023b
Hung Phan <phandinhhungvp2001@gmail.com>
2018-10-05 12:53:49 +0700

    remove project pathmap

--- Android.mk
+++ Android.mk
@@ -2,14 +2,11 @@ ifeq ($(TARGET_QCOM_MEDIA_VARIANT),caf-msm8996)
 
 QCOM_MEDIA_ROOT := $(call my-dir)
 
-#Compile these for all targets under QCOM_BOARD_PLATFORMS list.
-ifeq ($(call is-board-platform-in-list, $(QCOM_BOARD_PLATFORMS)),true)
+
 include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
 include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
 include $(QCOM_MEDIA_ROOT)/libaac/Android.mk
-endif
 
-ifeq ($(call is-board-platform-in-list, $(MSM_VIDC_TARGET_LIST)),true)
 include $(QCOM_MEDIA_ROOT)/mm-video-v4l2/Android.mk
 
 ifeq ($(BOARD_USES_ADRENO), true)
@@ -20,8 +17,6 @@ include $(QCOM_MEDIA_ROOT)/hypv-intercept/Android.mk
 
 ifeq ($(TARGET_BOARD_AUTO),true)
 include $(QCOM_MEDIA_ROOT)/libsidebandstreamhandle/Android.mk
-endif
-
 endif
 
 endif
